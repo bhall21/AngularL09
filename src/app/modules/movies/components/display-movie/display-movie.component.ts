07f1baf9b5fdc0aec8f3892e6ca06fab6d94462b
Brad Hall <bhall228@gmail.com>
2022-03-11 01:16:03 -0800

    first commit

---
+++ src/app/modules/movies/components/display-movie/display-movie.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { Movie } from '../../../../shared/models/movie'
+
+@Component({
+  selector: 'app-display-movie',
+  templateUrl: './display-movie.component.html',
+  styleUrls: ['./display-movie.component.css']
+})
+export class DisplayMovieComponent implements OnInit {
+
+ @Input() movie: Movie;
+
+ @Output() movieDeleted : EventEmitter<number> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+onClickDelete() {
+  this.movieDeleted.emit(this.movie.id);
+}
+
+}
